e7433d68a53e384e9d84df22d9bb09374bd5a5e6
immacool <65336226+immacool@users.noreply.github.com>
2022-03-20 13:41:45 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 class CaseFile(models.Model):
+    class Meta:
+        order = '-pub_date'
+    
     _uuid = models.CharField(max_length=36, default='undefined')
     first_name = models.CharField(max_length=40, default='undefined')
     last_name = models.CharField(max_length=40, default='undefined')
